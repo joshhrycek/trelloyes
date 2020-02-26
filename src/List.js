1ccdf3cbbbcbb85912710628af678b980cd44748
Hrycek <HrycekJ@pennmedicine.upenn.edu>
2020-02-26 15:35:28 -0500

    added delete and add functionality

--- src/List.js
+++ src/List.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React from 'react';
+import Card from './Card'
 import './List.css';
 
-export default class List extends Component {
-    render() {
-        return (
-            <div className="List">
-                <header className="List-header">
-                    {this.props.header}
-                </header>
-                    <div className="List-cards">
-                        {this.props.cards.map(card =>
-                            <Card
-                            title={card.title}
-                            content={card.content}
-                            key={card.id}
-                        />    
-                        )}
-                    </div>
-            </div>
-        )
-    }
+export default function List(props) {
+  return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {props.cards.map((card) =>
+          <Card
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            content={card.content}
+            onClickDelete={props.onClickDelete}
+          />
+        )}
+        <button
+          type='button'
+          className='List-add-button'
+          onClick={() => props.onClickAdd(props.id)}
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
+}
+
+List.defaultProps = {
+  onClickAdd: () => {},
 }
